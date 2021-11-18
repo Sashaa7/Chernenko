1757c10f19303c1a85b2499aa1f41996d64e5f0f
Sashaa7 <podjdkh@gmail.com>
2021-11-18 15:00:42 +0200

    Fourth task

---
+++ HT_1/4.py
@@ -0,0 +1,8 @@
+
+#Write a script to convert decimal to hexadecimal
+ #       Sample decimal number: 30, 4
+  #      Expected output: 1e, 04
+
+
+a = int(input()) # write your number here
+print(hex(a))
