9c638879397fba8c6a7f4b08531918981d3480ca
Sashaa7 <podjdkh@gmail.com>
2021-11-23 19:41:31 +0200

    Third task

---
+++ HT_3/2.py
@@ -0,0 +1,9 @@
+#Користувачем вводиться початковий і кінцевий рік. 
+#Створити цикл, який виведе всі високосні роки в цьому проміжку (границі включно).
+
+first_year = int(input('Write first year:'))
+
+the_last_year = int(input('Write the last year:'))
+
+leap_year = first_year < a < the_last_year
+
---
+++ HT_3/3.py
@@ -0,0 +1,16 @@
+ #Написати функцiю season, яка приймає один аргумент — номер мiсяця (вiд 1 до 12), яка буде повертати пору року
+ # якiй цей мiсяць належить (зима, весна, лiто або осiнь)
+
+ write_month_number = int(input())
+
+if  1 <= write_month_number <= 2 or write_month_number == 12:
+  print('winter')
+
+if 3 <= write_month_number <= 5:
+  print('spring')
+
+if 6 <= write_month_number <= 8:
+  print('summer')
+
+if 9 <= write_month_number <= 11:
+  print('autumn')
