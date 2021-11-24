46107297f47d92acb74562c8bdb7d6d093d74735
Sashaa7 <podjdkh@gmail.com>
2021-11-24 19:37:07 +0200

    Second task

--- HT_3/2.py
+++ HT_3/2.py
@@ -1,9 +1,10 @@
 #Користувачем вводиться початковий і кінцевий рік. 
 #Створити цикл, який виведе всі високосні роки в цьому проміжку (границі включно).
 
-first_year = int(input('Write first year:'))
+first_year = int(input('Write the first year:'))
 
 the_last_year = int(input('Write the last year:'))
 
-leap_year = first_year < a < the_last_year
-
+for num in range(first_year,the_last_year + 1):
+    if all(num % 400 == 0 or num %  4 == 0 and num % 100 != 0 for i in range(first_year ,num)):
+       print (num)
