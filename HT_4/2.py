ecab43608e37680cc48a35ecec4f5c63be0be9cc
Sashaa7 <podjdkh@gmail.com>
2021-11-27 19:27:33 +0200

    Second task

--- HT_4/2.py
+++ HT_4/2.py
@@ -4,3 +4,11 @@
 # ці гроші додаються до суми вкладу і в наступному році на них також нараховуються відсотки).
 #  Параметр < percents > є необов'язковим і має значення по замовчуванню < 10 > (10%).
 #  Функція повинна принтануть і вернуть суму, яка буде на рахунку.
+
+def bank():
+	vklad = int(input('Write how much money your wont invest:'))
+	percents = int(input('How much percent  you will have:'))
+	years = int(input('How much years you have for invest:'))
+	earning =(vklad * ((1 + percents/100)**years))
+	print(earning)
+bank()
