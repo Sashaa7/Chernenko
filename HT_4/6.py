3fc1646c4c1d1f059b5a979e740bac5793bbfbc1
Sashaa7 <podjdkh@gmail.com>
2021-11-27 23:56:29 +0200

    Sixth task

--- HT_4/6.py
+++ HT_4/6.py
@@ -1,2 +1,12 @@
 #Вводиться число. Якщо це число додатне, знайти його квадрат,
 #якщо від'ємне, збільшити його на 100, якщо дорівнює 0, не змінювати.
+number = int(input('Write your number:'))
+
+def our_function():
+	if number < 0:
+		print(number + 100)
+	if number > 0:
+		print(number ** 2)
+	if number == 0:
+		print(number)
+our_function()
