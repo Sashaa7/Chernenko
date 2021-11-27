e5acb5afb843202683fed3620ba2ee7c5bd695fd
Sashaa7 <podjdkh@gmail.com>
2021-11-27 21:44:34 +0200

    Third task

--- HT_4/3.py
+++ HT_4/3.py
@@ -1,2 +1,15 @@
 #Написати функцию < is_prime >, яка прийматиме 1 аргумент - число від 0 до 1000,
 # и яка вертатиме True, якщо це число просте, и False - якщо ні.
+number = int(input('Write number:'))
+
+
+if 1 < number < 1000:
+    for i in range(2, int(number/2)+1):
+         if (number % i) == 0:
+            print("false")
+            break
+    else:
+        print("true")
+
+else:
+    print("error")
