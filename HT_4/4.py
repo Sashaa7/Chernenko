99de0cb17844394c6f7cb0e8ea16478dff4ab5d6
Sashaa7 <podjdkh@gmail.com>
2021-11-27 23:48:47 +0200

    Fourth task

--- HT_4/4.py
+++ HT_4/4.py
@@ -1,2 +1,19 @@
 #Написати функцію < prime_list >, яка прийматиме 2 аргументи - початок і кінець діапазона,
 # і вертатиме список простих чисел всередині цього діапазона.
+
+first_number = int(input())
+second_number = int(input())
+new_list = []
+
+def prime_list():
+  for i in range(first_number, second_number):
+ 
+      for j in range(2,i):
+          if i%j==0:
+              break
+ 
+      else:  
+          new_list.append(i)
+ 
+  print(new_list)
+prime_list()
