e5bcaecf7ba16768ff1c52e45e4797c35e0bd479
Sashaa7 <podjdkh@gmail.com>
2021-11-28 21:40:55 +0200

    Seventh task

--- HT_4/7.py
+++ HT_4/7.py
@@ -1,1 +1,9 @@
 #Написати функцію, яка приймає на вхід список і підраховує кількість однакових елементів у ньому.
+def our_function():
+  our_list = input('Write your list:')
+  changed_list = set(our_list)
+  result={}
+  for i in changed_list:
+      result[i]=our_list.count(i)
+  print(result)
+our_function()
