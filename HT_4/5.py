da2cfbccac66392e544ec9a27bce5fa3d43dffce
Sashaa7 <podjdkh@gmail.com>
2021-11-28 20:30:40 +0200

    Fiveth task

--- HT_4/5.py
+++ HT_4/5.py
@@ -1,1 +1,16 @@
-#Написати функцію < fibonacci >, яка приймає один аргумент і виводить всі числа Фібоначчі, що не перевищують його.
+# Написати функцію < fibonacci >, яка приймає один аргумент і виводить всі числа Фібоначчі, що не перевищують його.
+
+def fibonacci():
+	fib_1 = 1
+	fib_2 = 1
+	fib_all = fib_1 + fib_2
+	number = int(input('Write number:'))
+	if number > 0:
+		print(fib_1)
+		print(fib_2)
+	while fib_all <= number:
+		print(fib_all)
+		fib_1 = fib_2
+		fib_2 = fib_all
+		fib_all = fib_1 + fib_2
+fibonacci()
