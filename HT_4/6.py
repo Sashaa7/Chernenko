1162ff8ad5c2f9cd20af562213b57c8143041c44
Sashaa7 <podjdkh@gmail.com>
2021-11-29 18:39:25 +0200

    Updated task

--- HT_4/6.py
+++ HT_4/6.py
@@ -2,11 +2,11 @@
 #якщо від'ємне, збільшити його на 100, якщо дорівнює 0, не змінювати.
 number = int(input('Write your number:'))
 
-def our_function():
+def our_function(number):
 	if number < 0:
 		print(number + 100)
 	if number > 0:
 		print(number ** 2)
 	if number == 0:
 		print(number)
-our_function()
+our_function(number)
