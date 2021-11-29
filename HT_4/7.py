7e173c35272d15175a4d327f3b88a7369e01f14f
Sashaa7 <podjdkh@gmail.com>
2021-11-29 18:14:18 +0200

    Updated task

--- HT_4/7.py
+++ HT_4/7.py
@@ -1,6 +1,6 @@
 #Написати функцію, яка приймає на вхід список і підраховує кількість однакових елементів у ньому.
 def our_function():
-  our_list = input('Write your list:')
+  our_list = [1, 2, 5, 7, 11, 2, 5, 6, 5, 5, "a", "b", "a", (1, 2), (1, 2), (5, "s")]
   changed_list = set(our_list)
   result={}
   for i in changed_list:
