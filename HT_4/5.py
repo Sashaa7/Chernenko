a0687c09eb5981f573bb52f0a437e7d87f5034d3
Sashaa7 <podjdkh@gmail.com>
2021-11-29 18:41:25 +0200

    Updated task

--- HT_4/5.py
+++ HT_4/5.py
@@ -1,10 +1,9 @@
 # Написати функцію < fibonacci >, яка приймає один аргумент і виводить всі числа Фібоначчі, що не перевищують його.
-
-def fibonacci():
+number = int(input('Write number:'))
+def fibonacci(number):
 	fib_1 = 1
 	fib_2 = 1
 	fib_all = fib_1 + fib_2
-	number = int(input('Write number:'))
 	if number > 0:
 		print(fib_1)
 		print(fib_2)
@@ -13,4 +12,4 @@ def fibonacci():
 		fib_1 = fib_2
 		fib_2 = fib_all
 		fib_all = fib_1 + fib_2
-fibonacci()
+fibonacci(number)
