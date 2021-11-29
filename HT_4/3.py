ac106a04b901ee61f6d0e895c3fbb05c2766e5fd
Sashaa7 <podjdkh@gmail.com>
2021-11-29 18:09:25 +0200

    Updated task

--- HT_4/3.py
+++ HT_4/3.py
@@ -2,14 +2,15 @@
 # и яка вертатиме True, якщо це число просте, и False - якщо ні.
 number = int(input('Write number:'))
 
+def is_prime():
+ if 1 < number < 1000:
+     for i in range(2, int(number/2)+1):
+          if (number % i) == 0:
+             print("false")
+             break
+     else:
+         print("true")
 
-if 1 < number < 1000:
-    for i in range(2, int(number/2)+1):
-         if (number % i) == 0:
-            print("false")
-            break
-    else:
-        print("true")
-
-else:
-    print("error")
+ else:
+     print("error")
+is_prime()
