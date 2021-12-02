bde21c87f3a51671d100cf26c4b6ca60da90f554
Sashaa7 <podjdkh@gmail.com>
2021-12-02 19:35:04 +0200

    Task

---
+++ HT_1/6.py
@@ -0,0 +1,10 @@
+user_number = int(input())
+
+result = ""
+
+for b in range(user_number):
+	print(b)
+	user_input = input("Enter") 
+	result = result + user_input
+
+print(result)
--- HT_5/2.py
+++ HT_5/2.py
@@ -3,3 +3,12 @@
  #  - пароль повинен бути не меншим за 8 символів і повинен мати хоча б одну цифру;
  #  - щось своє :)
  #  Якщо якийсь із параментів не відповідає вимогам - породити виключення із відповідним текстом.
+
+def our_function():
+  name = input('Please write your name:')
+  password = input('Write your password:')
+  if 3 < len(name) < 50 and if 8 < len(password):
+    print('Data is entered correctly')
+  else:
+    
+our_function()
